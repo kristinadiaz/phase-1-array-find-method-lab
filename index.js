800195a5bf3eea38d860b301cc219096def65bc0
Kristina Diaz <kristina.diaz84@gmail.com>
2021-11-04 16:07:27 -0600

    commit tree 7b7fc48ada36dac1b34fec6f31834edd73ff34fb

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+// function hasWin(obj) {
+//     if (obj.result === 'W') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
+// function superbowlWin(teamData) {
+//     const win = teamData.find(hasWin);
+
+//     if (win) {
+//     return win.year;
+//     } else {
+//         return undefined;
+//     }
+// }
+
+function superbowlWin(teamData) {
+    const win = teamData.find(obj => obj.result === 'W');
+    return win ? win.year : undefined;
+}
